cf50b3fecd32d40121e99701e50cd2b18598caa0
chandan222 <chandankalyani.c@gmail.com>
2017-09-13 14:35:40 +0530

    Upload Folder 3

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -273,7 +273,7 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getNearBirthDate(Date date, int days) {
 		// Add your implementation here
-		return null;
+		return students;
 	}
 
 	@Override
